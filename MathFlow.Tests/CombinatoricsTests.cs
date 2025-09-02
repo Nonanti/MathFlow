049a13a1ac8e920d245d29ee1dd48a059584f1e5
Garrison Smith <garrisonsmith11@gmail.com>
2025-09-01 20:25:48 -0700

    Adding combinatorics functions: binomial and permutation. Moving GCD, LCM, and factorial to CombinatoricsFunctions class. Added some unit tests.

---
+++ MathFlow.Tests/CombinatoricsTests.cs
@@ -0,0 +1,31 @@
+﻿using MathFlow.Core;
+
+namespace MathFlow.Tests;
+public class CombinatoricsTests
+{
+	private readonly MathEngine _engine;
+
+	public CombinatoricsTests()
+	{
+		_engine = new MathEngine();
+	}
+
+	[Theory]
+	[InlineData("gcd(12, 18)", 6)]
+	[InlineData("lcm(12, 18)", 36)]
+	[InlineData("factorial(5)", 120)]
+	[InlineData("perm(5, 3)", 60)]
+	[InlineData("Permutation(5, 3)", 60)]
+	[InlineData("perm(4, 2)", 12)]
+	[InlineData("perm(6, 0)", 1)]
+	[InlineData("perm(0, 0)", 1)]
+	[InlineData("binomial(5, 3)", 10)]
+	[InlineData("binomial(6, 0)", 1)]
+	[InlineData("ncr(6, 6)", 1)]
+	[InlineData("choose(6, 2)", 15)]
+	public void Calculate_CombinatoricsFunctions_ReturnsCorrectResult(string expression, double expected)
+	{
+		var result = _engine.Calculate(expression);
+		Assert.Equal(expected, result, 10);
+	}
+}
